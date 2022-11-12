9b21a17e3790efbc2054e74fd9f8ca1947da73ae
Ozenc Celik <ozenc.celik@hotmail.com>
2022-11-12 13:46:56 +0300

    * tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/views/*"],
+  content: [
+    "./src/views/*",
+    "./src/views/forms/*"
+  ],
   theme: {
     extend: {},
     fontFamily: {
