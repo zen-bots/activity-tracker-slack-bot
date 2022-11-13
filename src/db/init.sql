7582f85a2b8c8768d4716ed90de85c09b6b7f6f8
Ozenc Celik <ozenc.celik@hotmail.com>
2022-11-13 11:35:06 +0300

    * New product images added to explain what it's doing to users.

--- src/db/init.sql
+++ src/db/init.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS public.activity; CREATE TABLE public.activity (     id SERIAL NOT NULL,     name character varying(50) NOT NULL,     multiplication_factor numeric(6,2) NOT NULL,     CONSTRAINT activity_pkey PRIMARY KEY (id),     CONSTRAINT activity_name_key UNIQUE (name) );
 DROP TABLE IF EXISTS public.users; CREATE TABLE public.users (     id serial NOT NULL,     slack_id character varying(15) NOT NULL,     name character varying(50)  NOT NULL,     CONSTRAINT users_pkey PRIMARY KEY (id),     CONSTRAINT users_name_key UNIQUE (name),     CONSTRAINT users_slack_id_key UNIQUE (slack_id) );
 DROP TABLE IF EXISTS public.user_activity; CREATE TABLE public.user_activity (     id serial NOT NULL,     user_id integer NOT NULL,     activity_id integer NOT NULL,     point numeric(6,2) NOT NULL,     created_on timestamp without time zone NOT NULL,     CONSTRAINT user_activity_pkey PRIMARY KEY (id),     CONSTRAINT user_activity_user_id_fkey FOREIGN KEY (user_id)         REFERENCES public.users (id) MATCH SIMPLE         ON UPDATE NO ACTION         ON DELETE NO ACTION );
+--DROP TABLE IF EXISTS public.waitlist; CREATE TABLE public.waitlist (     id serial NOT NULL,     email_from character varying(100) NOT NULL,     email_to character varying(100) NOT NULL,     name character varying(50) NOT NULL,     message character varying(50) NOT NULL,     created_on timestamp without time zone NOT NULL );
 
 
 -- Table: public.activity
